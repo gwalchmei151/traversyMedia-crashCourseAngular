2e3f22fc048dc979bfeb41d025856d20ea286b33
Tushar Ismail <gwalchmei@gmail.com>
2021-05-13 00:38:13 +0800

    routing done. about and footer done. tutorial complete. will need to do again at a later stage when personal understanding is... better

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { UiService } from "../../services/ui.service";
 import {Subscription} from "rxjs";
+import { Router } from "@angular/router";
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
@@ -11,7 +12,7 @@ export class HeaderComponent implements OnInit {
   showAddTask: boolean;
   subscription: Subscription;
 
-  constructor(private uiService:UiService) {
+  constructor(private uiService:UiService, private router:Router) {
     this.subscription = this.uiService.onToggle().subscribe(value => this.showAddTask = value)
    }
 
@@ -20,6 +21,10 @@ export class HeaderComponent implements OnInit {
 
   toggleAddTask() {
     this.uiService.toggleAddTask();
+  }
+
+  hasRoute(route: string) {
+    return this.router.url === route;
   }
 
 }
